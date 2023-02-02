014cc7f3c5b94d54057b652b16f110891bb90c85
crips24 <92549357+crips24@users.noreply.github.com>
2023-02-02 20:12:18 +0100

    Le début !
    le début de la démo jouable

---
+++ Assets/scripts/Move_player.cs
@@ -0,0 +1,23 @@
+
+using UnityEngine;
+
+public class Move_player : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+
+
+
+    }
+
+
+    void Update()
+    {
+        
+
+
+        
+    }
+}
